724beb5ed91444807ab8aa47585f69e9b701d742
jobet1995 <jobetcasquejo221@gmail.com>
2025-11-15 19:24:25 +0800

    'Initialize'

---
+++ brix/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-xfyh$a0!)984x^6s6k)8hoi8h(92w2q^52s(y%a%j!0$u+8we="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
